edcfc603ab8360ecc1cbb449647ab8cb7b58e14d
Bogdan Paul <Crash_BPS@yahoo.com>
2023-04-25 21:45:55 +0300

    added player controls camera enemies tilemaps

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D rigidBody;
+    BoxCollider2D boxCollider;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    private void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(MathF.Sign(rigidBody.velocity.x)), 1f);
+    }
+}
